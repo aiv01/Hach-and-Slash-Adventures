901ff47ccce1f64479ebb37744ea5070462c78bb
Stefano Canavesi <ultratails2001@gmail.com>
2022-04-06 17:27:00 +0200

    Removed debug log

--- HackAndSlash/Assets/Scripts/MenuScripts/PauseMenuController.cs
+++ HackAndSlash/Assets/Scripts/MenuScripts/PauseMenuController.cs
@@ -94,10 +94,8 @@ public class PauseMenuController : MonoBehaviour
         if (!hasChangedSelection)
         {
             currentSelection -= (int)input.GetAxisRaw(skillChangeAxis);
-            Debug.Log((int)input.GetAxisRaw(skillChangeAxis));
             hasChangedSelection = true;
             currentSelection = currentSelection >= (int)PauseMenuOption.last ? (int)PauseMenuOption.Resume : currentSelection < 0 ? (int)PauseMenuOption.Quit : currentSelection;
-            Debug.Log(currentSelection);
         }
         if (input.GetAxisRaw(skillChangeAxis) == 0)
         {
