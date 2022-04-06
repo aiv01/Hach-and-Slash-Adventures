aec389c6f059a822870503f2ee1bd2330304628f
Stefano Canavesi <ultratails2001@gmail.com>
2022-04-06 20:10:03 +0200

    Bigger enemy range

--- HackAndSlash/Assets/Scripts/HitDetection/HurtboxBehavior.cs
+++ HackAndSlash/Assets/Scripts/HitDetection/HurtboxBehavior.cs
@@ -7,11 +7,19 @@ public class HurtboxBehavior : MonoBehaviour
 {
     [SerializeField] private string canGetHitBy;
     [SerializeField] private TextMeshPro textPrefab;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip[] hitAudio;
+
+    private void Awake() {
+        audioSource = GetComponentInParent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag(canGetHitBy)) {
             Debug.Log("hit");
             CharacterStats otherStats = other.GetComponentInParent<CharacterStats>();
             CreateDamageText(otherStats.DealDamage(GetComponentInParent<CharacterStats>(), otherStats.equippedWeapon.damageType).ToString());
+            audioSource.clip = hitAudio[Random.Range(0, hitAudio.Length)];
+            audioSource.Play();
         }
     }
 
