c5d7771bcd3c27c2a4fe5fd9b27900e9310a3968
Stefano Canavesi <ultratails2001@gmail.com>
2022-04-09 17:33:52 +0200

    Disappering enemies

--- HackAndSlash/Assets/Scripts/Enemy/EnemyLogic.cs
+++ HackAndSlash/Assets/Scripts/Enemy/EnemyLogic.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterStats))]
-public class EnemyLogic : MonoBehaviour
-{
+public class EnemyLogic : MonoBehaviour {
     [Header("Level")]
     [SerializeField] public int level = 1;
-    
+
+    [Header("Dead")]
+    private bool dead;
+
     [Header("References")]
     public CharacterStats enemyStats;
+    [SerializeField] private GameObject[] disableOnDeath;
+    [SerializeField] private Animator anim;
+    [SerializeField] private SkinnedMeshRenderer mr;
+    [SerializeField] private Material dissolveMaterial;
+    private Material[] originalMaterials;
+    [SerializeField] private float disappearSpeed;
+    private float disappearingTime;
+    private MaterialPropertyBlock dissolveProperties;
 
     // Start is called before the first frame update
     private void Awake() {
         enemyStats = GetComponent<CharacterStats>();
+        anim = GetComponent<Animator>();
+        mr = GetComponentInChildren<SkinnedMeshRenderer>();
+        dissolveProperties = new MaterialPropertyBlock();
+        originalMaterials = mr.materials;
     }
 
     private void OnEnable() {
+        Reactivate();
         enemyStats.InitializeCharacter();
         BalanceToLevel();
     }
@@ -32,9 +47,18 @@ public class EnemyLogic : MonoBehaviour
     }
 
     private void Update() {
-        if (enemyStats.hp <= 0) {
+        if (enemyStats.hp <= 0 && !dead) {
             Die();
-        }   
+        }
+        if (dead) {
+            disappearingTime += Time.deltaTime * disappearSpeed;
+            mr.GetPropertyBlock(dissolveProperties);
+            dissolveProperties.SetFloat("_DissolveAmount", Mathf.Lerp(0, 1, disappearingTime));
+            mr.SetPropertyBlock(dissolveProperties);
+            if (disappearingTime >= 1) {
+                transform.parent.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void HitStart() {
@@ -44,6 +68,22 @@ public class EnemyLogic : MonoBehaviour
     private void Die() {
         PlayerLogic.Instance.GetExp(enemyStats.exp);
         DataManagement.enemyKilled++;
-        transform.parent.gameObject.SetActive(false);
+        for (int i = 0; i < disableOnDeath.Length; i++) {
+            disableOnDeath[i].SetActive(false);
+        }
+        Material[] mats = new Material[] { dissolveMaterial, dissolveMaterial };
+        mr.materials = mats;
+        disappearingTime = 0;
+        anim.speed = 0;
+        dead = true;
+    }
+
+    private void Reactivate() {
+        dead = false;
+        anim.speed = 1;
+        for (int i = 0; i < disableOnDeath.Length; i++) {
+            disableOnDeath[i].SetActive(true);
+        }
+        mr.materials = originalMaterials;
     }
 }
