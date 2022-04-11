61084476aec94af9aedde20f9ec92c76537a9ac8
Stefano Canavesi <ultratails2001@gmail.com>
2022-04-11 18:24:20 +0200

    Game over fade

--- HackAndSlash/Assets/Scripts/MenuScripts/GameOverMenuController.cs
+++ HackAndSlash/Assets/Scripts/MenuScripts/GameOverMenuController.cs
@@ -2,24 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float fadeSpeed;
+    private float currentFade;
+    [SerializeField] private Transform gameOverText;
+    private Image gameOverBlack;
+
+    private void Awake() {
+        gameOverBlack = GetComponent<Image>();
     }
 
+    private void OnEnable() {
+        currentFade = 0;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        currentFade += Time.deltaTime * fadeSpeed;
+        gameOverBlack.color = new Color(
+            gameOverBlack.color.r,
+            gameOverBlack.color.g,
+            gameOverBlack.color.b,
+            Mathf.Lerp(0, 1, currentFade)
+            );
+        if(currentFade >= 1) {
+            gameOverText.gameObject.SetActive(true);
+        }
     }
 
     public void Restart()
     {
-        SceneManager.LoadScene("PrototypeAmbientScene");
+        DataManagement.Load();
     }
 
     public void QuitGame()
