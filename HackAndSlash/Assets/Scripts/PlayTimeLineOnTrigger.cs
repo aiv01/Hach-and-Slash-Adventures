e9a317f17709bac40a905ca1c12e9d670a9b4f9f
Stefano Canavesi <ultratails2001@gmail.com>
2022-04-14 17:42:16 +0200

    Fixed cutscene playing on load

--- HackAndSlash/Assets/Scripts/PlayTimeLineOnTrigger.cs
+++ HackAndSlash/Assets/Scripts/PlayTimeLineOnTrigger.cs
@@ -2,26 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class PlayTimeLineOnTrigger : MonoBehaviour
 {
     bool played;
     public PlayableDirector timeLine;
+    [SerializeField] private PlayableDirector activatedTimeline;
     [SerializeField] private string key;
+    [SerializeField] private int trackIndex;
+    private TimelineAsset activatedAsset;
 
     private void Awake() {
+        activatedAsset = (TimelineAsset)activatedTimeline.playableAsset;
         DataManagement.SetKey(key, false);
     }
 
     private void Update() {
+        if (activatedTimeline.time >= activatedTimeline.duration) {
+            DataManagement.SetKey(key, true);
+        }
         if (DataManagement.GetKey(key)) {
+            MuteTrack(trackIndex);
             PlayTimeLine();
+        }
+        else {
+            UnmuteTrack(trackIndex);
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         PlayTimeLine();
-        DataManagement.SetKey(key, true);
     }
 
     private void PlayTimeLine() {
@@ -30,5 +41,15 @@ public class PlayTimeLineOnTrigger : MonoBehaviour
         }
         played = true;
         timeLine.Play();
+    }
+
+    private void MuteTrack(int index) {
+        TrackAsset track = activatedAsset.GetOutputTrack(index);
+        track.muted = true;
+    }
+
+    private void UnmuteTrack(int index) {
+        TrackAsset track = activatedAsset.GetOutputTrack(index);
+        track.muted = false;
     }
 }
